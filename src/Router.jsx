3c29f7635f15994039a8e276ed84b931c1f99416
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-08 11:34:37 +0600

    first commit

---
+++ src/Router.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello Mair!</div>,
+  },
+]);
+export default router;
