5e2ef3d97941b0b5c874fcb188a31b1ae479fc81
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-08 18:12:58 +0600

    Installation of Tailwind and DaisyUi

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello Mair!</div>,
+    element: <Home></Home>
   },
 ]);
 export default router;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div className="badge badge-outline">default</div>
+      <div className="badge badge-primary badge-outline">primary</div>
+      <div className="badge badge-secondary badge-outline">secondary</div>
+      <div className="badge badge-accent badge-outline">accent</div>
+    </div>
+  );
 };
 
 export default Home;
